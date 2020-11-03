f41a5057da53247c61c16666cd2b1e40ab33a274
jayant-ism <52823326+jayant-ism@users.noreply.github.com>
2020-11-03 19:52:06 +0530

    as

---
+++ blog/urls.py
@@ -0,0 +1,31 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="blog-home") ,
+    path('campgrounds-index', views.campgroundsindex, name="campgroundsindex") ,
+    path('campgroundopen', views.campgroundopen, name="campgroundopen") ,
+    path('campgroundaddcom', views.campgroundaddcom , name="campgroundaddcom") ,
+    path('login', views.login, name="login"),
+    path('register', views.register, name="register") ,
+    path('control', views.control, name="control") ,
+    path('comallo', views.comallo, name="comallo") ,
+    path('comrem', views.comrem, name="comrem") ,
+    
+    path('add', views.add, name="add") ,
+    path('submit', views.submit, name="submit") ,
+    path('userdetails', views.userdetails, name="userdetails") ,
+    path('logout', views.logouts, name="logouts") ,
+    path('changepass', views.changepass, name="changepass") ,
+    path('descadd', views.descadd, name="descadd") ,
+    path('imgchange', views.imgchange , name="imgchange") ,
+    path('iconchange', views.iconchange , name="iconchange") ,
+    
+    path('search', views.search , name="search") ,
+    
+    path('deletepost', views.deletepost , name="deletepost") ,
+    
+    
+ 
+
+]
